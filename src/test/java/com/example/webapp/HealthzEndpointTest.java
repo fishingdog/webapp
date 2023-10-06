bd931f59af44f00c4c49bb178bb619f049742fc6
Beluga_samsung <2x3tech@gmail.com>
2023-10-06 11:30:43 -0700

    writing yml

--- src/test/java/com/example/webapp/HealthzEndpointTest.java
+++ src/test/java/com/example/webapp/HealthzEndpointTest.java
@@ -2,23 +2,22 @@ package com.example.webapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HealthzEndpointTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private TestRestTemplate restTemplate;
 
     @Test
-    public void testHealtzEndpoint() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/healthz"))
-                .andExpect(status().isOk());
+    public void checkHealthReturns200() {
+        ResponseEntity<Void> response = restTemplate.getForEntity("/healthz", Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
